10cdad09b50cf268ba2828b362b60bb6546cf537
Antara Ganapathy <antaraganapathy@gmail.com>
2024-09-06 12:59:28 +0530

    installing libs

---
+++ rag.py
@@ -0,0 +1,13 @@
+from langchain.document_loaders import TextLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.llms import HuggingFaceHub
+from dotenv import load_dotenv
+from data import pdf_to_text
+import os
+from langchain.vectorstores import Pinecone
+from pinecone import Pinecone as pinecone
+from pinecone import ServerlessSpec
+from langchain import PromptTemplate
+from langchain.schema.runnable import RunnablePassthrough
+from langchain.schema.output_parser import StrOutputParser
