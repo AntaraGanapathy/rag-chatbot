55057955eef6a903edb43441cafd54f0d94c4ef7
Antara Ganapathy <antaraganapathy@gmail.com>
2024-09-07 11:16:24 +0530

    st msg

--- main.py
+++ main.py
@@ -11,3 +11,11 @@ with st.sidebar:
 def generate_response(input):
     result = bot.rag_chain.invoke(input)
     return result.split("Answer:", 1)[1]
+
+
+if "messages" not in st.session_state.keys():
+    st.session_state.messages = [{"role": "assistant", "content": "Welcome, let's unveil your future"}]
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
